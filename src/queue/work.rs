1e62c9632ebb2c5b8e5e794fedac75c84572e784
Adam Siwiec <adamsiwiec01@gmail.com>
2023-06-07 19:26:46 -0700

    feat(wasm): Add wasm client

---
+++ src/queue/work.rs
@@ -0,0 +1,23 @@
+// use std::sync::{Arc, Mutex};
+
+use crate::{
+    data::{EntryID, Layout, SlotMetaTile, SlotTile, SummaryTile, TileID},
+    timestamp::Interval,
+};
+
+// make a template struct called Data
+#[derive(Clone, Debug, PartialEq)]
+pub enum Data<T> {
+    Requested,
+    Ready(T),
+}
+
+#[derive(Clone, Debug)]
+pub enum Work {
+    FetchInfo(Data<Layout>),
+    FetchSlotMetaTile(EntryID, TileID, Data<SlotMetaTile>),
+    FetchSlotTile(EntryID, TileID, Data<SlotTile>),
+    FetchTiles(EntryID, Interval, Data<Vec<TileID>>),
+    FetchSummaryTile(EntryID, TileID, Data<SummaryTile>),
+    FetchInterval(Data<Interval>),
+}
