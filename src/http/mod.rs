8a7a98bc6ed34f254000656852ac27dfcb9b0954
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-06-09 10:10:06 -0700

    Remove fetch from this patchset.

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -2,14 +2,3 @@ pub mod schema;
 
 #[cfg(feature = "server")]
 pub mod server;
-
-#[cfg(feature = "client")]
-pub mod fetch;
-
-#[cfg(feature = "client")]
-#[cfg(not(target_arch = "wasm32"))]
-pub mod fetch_native;
-
-#[cfg(feature = "client")]
-#[cfg(target_arch = "wasm32")]
-pub mod fetch_web;
