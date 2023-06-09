24a8dc9dd6d094ad46376049b2a2e8e69f22ae50
Elliott Slaughter <slaughter@cs.stanford.edu>
2023-06-09 11:11:38 -0700

    Refactor.

--- src/http/schema.rs
+++ src/http/schema.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::data::{EntryID, TileID};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct TileRequest {
     pub entry_id: EntryID,
     pub tile_id: TileID,
